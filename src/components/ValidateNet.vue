ccb7d2c5e6814dad72f9fb3448c392b59865d071
szhelonkin <zhelonkin@gravio.net>
2023-11-14 17:19:41 +0300

    development

---
+++ src/components/ValidateNet.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grid">
+    <div class="col">
+      <img v-bind:src="img" height="100" class="mr-2"/>
+    </div>
+    <div class="col">
+      <div class="text-3xl">{{ name }}</div>
+      <div class="text-900">APR ~{{ apr }}</div>
+      <p/>
+      <div>
+        <a v-bind:href="stake" target="_blank" rel="noopener noreferrer">
+          <button-click class="bg-orange-500" label="Stake" rounded />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    apr: String,
+    img: String,
+    stake: String
+  }
+};
+</script>
