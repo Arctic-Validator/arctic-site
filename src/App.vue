b6f7e73fc2eb356dea31a80ce1da335cd2f01be0
szhelonkin <zhelonkin@gravio.net>
2023-11-14 17:15:05 +0300

    dev

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--h1>Arctic</h1-->
+  <div class="bg-bluegray-900 h-screen flex align-items-center justify-content-center">
+    <div class="grid" style="max-width: 1000px">
+      <div class="col-12">
+        <div class="card relative flex z-2 surface-0 shadow-2 p-3 border-round-lg align-items-center justify-content-center">
+          <img :src="'arctic.png'" height="100" class="mr-2"/>
+          <div class="card">
+            <div class="text-6xl">Arctic</div>
+            <div class="text-2xl">Validator</div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="card relative flex z-2 surface-0 shadow-2 p-3 border-round-lg align-items-center justify-content-center">
+          <p>ARCTIC validators work on our own servers in a reliable data center. We are support decentralization.</p>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card relative flex z-2 surface-0 shadow-2 p-3 border-round-lg">
+          <validate-net name="Band" apr="10%" img="band.png" stake="https://ping.pub/band/staking/bandvaloper1c34u63fdagur6xfmnu2d50242d3kszl9wnulk8"/>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card relative flex z-2 surface-0 shadow-2 p-3 border-round-lg">
+          <validate-net name="Evmos" apr="36%" img="evmos.jpeg" stake="https://ping.pub/evmos/staking/evmosvaloper1tk9m4drzupjpsdfr99t5pkhtl0w57ykpvvsvcm"/>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card relative flex z-2 surface-0 shadow-2 p-3 border-round-lg">
+          <validate-net name="Bitcanna" apr="40%" img="bitcanna.svg" stake="https://ping.pub/bitcanna/staking/bcnavaloper1r0cvwdwkz24qm390cvdx68uxedt6ua77l5d7uq"/>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="card relative z-2 surface-0 shadow-2 p-3 border-round-lg">
+          <div class="text-2xl flex">Team</div>
+          <div class="flex">
+            <chip-show label="HalfBear" image="halfbear.jpg"/>
+            <chip-show label="Hilonom" image="hilonom.png"/>
+          </div> 
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="card relative flex z-2 surface-0 shadow-2 p-3 border-round-lg">
+          <a href="https://github.com/Arctic-Validator" target="_blank" rel="noopener noreferrer">
+            <button-click icon="pi pi-github" severity="secondary" text rounded aria-label="Github" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -21,6 +65,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
+import Panel from 'primevue/panel'
+import Button from 'primevue/button';
+import Chip from 'primevue/chip';
+import ValidateNet from './components/ValidateNet.vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// import CSS resources
+import 'primevue/resources/primevue.min.css'            // core 
+//import '/node_modules/primeflex/themes/primeone-light.css'  // theme
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primeicons/primeicons.css'                      // icons
+import '/node_modules/primeflex/primeflex.css'          // PrimeFlex
+
+
+const app = createApp(App)
+app.component('view-panel', Panel)
+app.component('button-click', Button)
+app.component('chip-show', Chip)
+app.component('validate-net', ValidateNet)
+app.mount('#app')
